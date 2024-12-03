6b6b303ff15d3f7192ba5e2ac3d5589e0f6a61de
Brent Tunnicliff <brent@tunnicliff.dev>
2024-12-03 23:38:50 +1100

    Day 3 part 2

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -8,7 +8,45 @@ struct Day03: AdventDay {
     let data: String
 
     func part1() -> Int {
+        calculateValues(data)
+    }
+
+    func part2() -> Int {
+        // Lets split this into two regex.
+        // First one only captures the valid substrings.
+        // Then we apply the same capture logic as part one to each match.
         data.matches {
+            ChoiceOf {
+                Regex {
+                    Anchor.startOfSubject
+                    Capture(OneOrMore(.any, .reluctant), transform: { $0.toString() })
+                    "don't()"
+                }
+                Regex {
+                    "do()"
+                    Capture(OneOrMore(.any, .reluctant), transform: { $0.toString() })
+                    "don't()"
+                }
+                Regex {
+                    "do()"
+                    Capture(OneOrMore(.any, .reluctant), transform: { $0.toString() })
+                    Anchor.endOfSubject
+                }
+            }
+        }
+        .reduce(into: [String?]()) { partialResult, match in
+            partialResult.append(match.output.1)
+            partialResult.append(match.output.2)
+            partialResult.append(match.output.3)
+        }
+        .compactMap { $0 }
+        .reduce(into: 0) { partialResult, match in
+            partialResult += calculateValues(match)
+        }
+    }
+
+    private func calculateValues(_ input: String) -> Int {
+        input.matches {
             "mul("
             Capture(
                 ChoiceOf {
--- Tests/Day03Tests.swift
+++ Tests/Day03Tests.swift
@@ -5,17 +5,17 @@ import Testing
 @testable import AdventOfCode
 
 struct Day03Tests {
-    let testData = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-
     @Test
     func testPart1() {
+        let testData = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
         let challenge = Day03(data: testData)
         #expect(challenge.part1() == 161)
     }
 
-//    @Test
-//    func testPart2() {
-//        let challenge = Day03(data: testData)
-//        #expect(challenge.part2() == 32000)
-//    }
+    @Test
+    func testPart2() {
+        let testData = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+        let challenge = Day03(data: testData)
+        #expect(challenge.part2() == 48)
+    }
 }
